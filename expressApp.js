ff6a5e22701c04c480440a1eb9a1865b21e955f7
NamjinCho <skawls5028@gmail.com>
2017-07-03 17:20:26 +0900

    study

--- expressApp.js
+++ expressApp.js
@@ -9,7 +9,26 @@ app.get('/',function(req,res){ // rerquest 가 먼저 그리고 response
 
   res.send('hello express\n')
 })
+app.get('/topic/:id',function(req,res){
 
+  var strings = [
+    'Express is',
+    'Node js is',
+    'Javascript is',
+  ]
+  var links = `
+    <a href="/topic?id=0">Express</a><br>
+    <a href="/topic?id=1">Nodejs</a><br>
+    <a href="/topic?id=2">JavaScript</a><br>
+    ${strings[req.params.id]}
+  `
+  res.send(links);
+
+})
+app.get('/queryTest/:tid',function(req,res){
+
+  res.json(req.params);
+})
 app.get('/dynamic',function(req,res){
 
   var d='dynamic'
